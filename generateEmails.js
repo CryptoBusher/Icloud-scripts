cb216dd772db410e494b69dda933e29ddf6c4443
Crypto B <103379312+CryptoBusher@users.noreply.github.com>
2025-02-03 09:05:21 +0200

    Updated selectors

--- generateEmails.js
+++ generateEmails.js
@@ -13,20 +13,19 @@ const fillInput = (input, value) => {
 
 let batchIndex = 1;
 for (let i = startIndex; i<=endIndex; i++) {
-    let addBtn = document.getElementsByTagName('iframe')[0].contentWindow.document.querySelector('button[type="button"][title="Add"]');
+    let addBtn = document.querySelector('.AddButton > button')
     addBtn.click();
     await sleep(1000);
 
-    let labelInput = document.getElementsByTagName('iframe')[0].contentWindow.document.getElementsByTagName("input")[0];
+    let labelInput = document.getElementsByTagName("input")[0];
     fillInput(labelInput, i);
     await sleep(1000); 
 
-    let createNewEmailBtnXpath = "//button[contains(text(),'Create email address')]";
-    let createNewEmailBtn = document.getElementsByTagName('iframe')[0].contentWindow.document.getElementsByTagName("button")[2];
+    let createNewEmailBtn = document.getElementsByTagName("button")[2];
     createNewEmailBtn.click();
     await sleep(5000);
 
-    let backBtn = document.getElementsByTagName('iframe')[0].contentWindow.document.getElementsByTagName("button")[1];
+    let backBtn = document.getElementsByTagName("button")[1];
     backBtn.click();
     await sleep(1000);
     
